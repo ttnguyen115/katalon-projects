167a8a68a4c9f76829e23c597ab7c75e19e56497
ttnguyen115 <thanhtrung1012000@gmail.com>
2024-05-29 21:59:52 +0700

    add more more test case creations

---
+++ Object Repository/Webcomponents/Page_The Ultimate Guide For Designing A Pro_e3e128/a_Create account.rs
@@ -0,0 +1,154 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Create account</name>
+   <tag></tag>
+   <elementGuidId>fa81c452-474e-4b11-8dbb-cad9076de004</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='authentication-top-nav-actions']/a</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.c-cta.c-cta--branded.whitespace-nowrap.mr-2</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>a2667c49-6a79-47ec-95cb-9de8c48e0789</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/enter?state=new-user</value>
+      <webElementGuid>3a1b3a7f-6d46-472c-9774-4ea7fa1a4f9f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-tracking-id</name>
+      <type>Main</type>
+      <value>ca_top_nav</value>
+      <webElementGuid>fa19dfba-a7bd-4278-a81c-12092efd4de9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-tracking-source</name>
+      <type>Main</type>
+      <value>top_navbar</value>
+      <webElementGuid>e241b720-5a40-4647-8607-61b8a453afab</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>c-cta c-cta--branded whitespace-nowrap mr-2</value>
+      <webElementGuid>828cbe93-fe89-4517-aa42-5ddc92668856</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            Create account
+          </value>
+      <webElementGuid>278f90f0-5ae4-4e95-bd0a-781833e63651</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;authentication-top-nav-actions&quot;)/a[@class=&quot;c-cta c-cta--branded whitespace-nowrap mr-2&quot;]</value>
+      <webElementGuid>2bfb6099-8710-420e-8fc5-07b1f143f0cc</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='authentication-top-nav-actions']/a</value>
+      <webElementGuid>f122f9cc-1c2a-4c71-916c-871b741de84b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Create account')]</value>
+      <webElementGuid>d472bea2-9e34-4483-ad8c-5c2bbea2b18d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Log in'])[1]/following::a[1]</value>
+      <webElementGuid>f93c60fd-3593-4266-b719-f0408e268350</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='DEV Community'])[1]/preceding::a[1]</value>
+      <webElementGuid>13504580-628f-4296-9580-387fd9c81a76</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Add reaction'])[1]/preceding::a[1]</value>
+      <webElementGuid>3c60abb0-45af-47d2-af62-197f3ad0c7a5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Create account']/parent::*</value>
+      <webElementGuid>80663134-3332-4bd9-b615-4b2600e130a2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/enter?state=new-user')]</value>
+      <webElementGuid>6ffe175b-8c7f-4387-ac30-043f8510f2b1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/a</value>
+      <webElementGuid>abb3696f-643f-4939-93fe-2cadcae97381</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/enter?state=new-user' and (text() = '
+            Create account
+          ' or . = '
+            Create account
+          ')]</value>
+      <webElementGuid>d4d2325b-beb8-4db6-8ab2-e827881191f3</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
